780fc952fa6cfbaa97ed536ee3b70c3e2746e804
Jenny Cha <jennycha@Jennys-MacBook-Pro.local>
2024-07-02 17:11:58 -0400

    Fixed the import error

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from '@emotion/react'
 
 import './themes/reset.css'
 import './themes/theme.css'
-import { lightTheme } from './themes/light.js'
+import { theme as lightTheme } from './themes/light.js'
 import App from './App.jsx'
 
 
