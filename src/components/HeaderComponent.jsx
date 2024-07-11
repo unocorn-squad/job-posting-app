57aa7b003388119840b4262b8d40e47f72d89f6f
Deepthi <deeptijnair@gmail.com>
2024-07-11 15:54:34 -0400

    added media queries

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,53 +1,68 @@
 import React from "react";
 import styled from "@emotion/styled";
-import header from "../assets/images/header/bg-pattern-header.svg";
+import Header from "../assets/images/header/bg-pattern-header.svg";
+import MobileHeader from "../assets/images/header/bg-pattern-header--mobile.svg";
+import TabletHeader from "../assets/images/header/bg-pattern-header--tablet.svg";
 
 
 
 const StyledContainer = styled.div`
 
   height: 228px;
-  width: 327px;
+  width: 100%;
+  display:flex;
   @media (min-width: ${(props) => props.theme.breakpoints.tablet_small}) {
-    width: 339px;
+    width:100%;
   }
   @media (min-width: ${(props) => props.theme.breakpoints.desktop_small}) {
-    width: 350px;
+  width: 100%;
+   
   }
   background-color: ${(props) => props.theme.colors.white};
-  display: flex;
   align-items: center;
   justify-items: center;
   position: relative;
 `;
-
 const StyledText = styled.div`
-  font-size: ${(props) => props.theme.typography.fs125};
+  font-size: ${(props) => props.theme.typography.fs175};
   font-weight: ${(props) => props.theme.typography.fwBold};
   color: ${(props) => props.theme.colors.white};
   z-index: 10;
   position: absolute;
   top: 30px;
-  margin-left : 200px;
+  margin-left : 80px;
 `;
 
-const HeaderImage = styled.img`
-  top : 0 ;
-  position: absolute;
-  
-
+const HeaderStyle = styled.div`
+  display:flex;
+  height:136px;
+  width:100%;
+  background: url('${MobileHeader}') no-repeat;
+  background-size:cover;
+  top:0;
+  position: absolute; 
+  @media (min-width: ${(props) => props.theme.breakpoints.tablet_small}) {
+    width:100%;
+    background: url('${TabletHeader}') no-repeat; 
+    background-size:cover;   
+    
+  }
+  @media (min-width: ${(props) => props.theme.breakpoints.desktop_small}) {
+    background: url('${Header}') no-repeat;  
+    }
 `;
+
+
+
 
 const HeaderComponent = () => {
     return (
 
         <StyledContainer>
-            <HeaderImage
-                src={header}
-                alt={'header'}
-                
-            />
-            <StyledText>devjobs</StyledText>
+            <HeaderStyle alt={'header'}>
+                <StyledText>devjobs</StyledText>
+
+            </HeaderStyle>
         </StyledContainer>
 
     );
