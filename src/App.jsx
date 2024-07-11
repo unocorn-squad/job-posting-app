a66bd1784ce08ab9d3ff22d2ee4a0b149618f27d
JC <16579792+jennyychaa@users.noreply.github.com>
2024-07-11 12:46:50 -0400

    Set-up the routes and created initial pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Error from './pages/Error';
+import JobDetails from './pages/JobDetails';
+import JobSearch from './pages/JobSearch';
+
 function App() {
   return (
-    <>Job Board Placeholder</>
-  )
+    <Routes>
+      <Route path='/' element={<JobSearch />} />
+      <Route path='jobs/:jobId' element={<JobDetails />} />
+      <Route path='*' element={<Error />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
--- src/pages/JobSearch/index.jsx
+++ src/pages/JobSearch/index.jsx
@@ -1,6 +1,6 @@
-function Search() {
+function JobSearch() {
   // @TODO Replace the placeholder content with appropriate components when they are complete.
-  return <>Search Page</>;
+  return <>Job Search Page</>;
 }
 
-export default Search;
+export default JobSearch;
