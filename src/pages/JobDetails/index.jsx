b90d568e00a67615aecf36b8cb7c1a4329801d15
JC <16579792+jennyychaa@users.noreply.github.com>
2024-07-11 10:57:00 -0400

    Created placeholder pages; deleted .gitkeep since it's no longer necessary

---
+++ src/pages/JobDetails/index.jsx
@@ -0,0 +1,6 @@
+function JobDetails() {
+  // @TODO Replace the placeholder content with appropriate components when they are complete.
+  return <>Job Details Page</>;
+}
+
+export default JobDetails;
---
+++ src/pages/Search/index.jsx
@@ -0,0 +1,6 @@
+function Search() {
+  // @TODO Replace the placeholder content with appropriate components when they are complete.
+  return <>Search Page</>;
+}
+
+export default Search;
