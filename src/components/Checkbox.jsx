851f5ca6057363c64bb6be92188127425c1cc155
jacquikeating <j.keating@live.ca>
2024-07-11 11:03:51 -0400

    Converted to Emotion styled component
    - Deleted components/Checkbox folder and original Checkbox.jsx and .css files
    - Created new Checkbox.jsx in components folder
    - Converted to Emotion
    - Added an isChecked state, an onChange listener, and a handleCheck function. Currently they do not do anything more than toggle the state, but may be useful later when integrating this component into the search form.

---
+++ src/components/Checkbox.jsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import styled from "@emotion/styled";
+
+const CheckboxContainer = styled.div`
+  display: none;
+
+  @media (min-width: ${(props) => props.theme.breakpoints.tablet_small}) {
+    display: flex;
+    gap: ${(props) => props.theme.layout.space200};
+    color: ${(props) => props.theme.colors.darkBlue};
+  }
+`;
+
+const CheckboxInput = styled.input`
+  outline: ${(props) => props.theme.colors.violet};
+  background-color: ${(props) => props.theme.colors.lightGray};
+  width: 24px;
+  height: 24px;
+  appearance: none;
+  border-radius: 3px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${(props) => props.theme.colors.lightGray};
+  }
+
+  &:checked {
+    background-color: ${(props) => props.theme.colors.violet};
+    background-size: 65%;
+    background-position: center 60%;
+    background-repeat: no-repeat;
+    background-image: url("/src/assets/images/icons/icon-check.svg");
+  }
+`;
+
+const CheckboxLabel = styled.label`
+  display: none;
+
+  @media (min-width: ${(props) => props.theme.breakpoints.tablet_small}) {
+    font-weight: ${(props) => props.theme.typography.fwBold};
+    display: inline;
+    span {
+      display: none;
+    }
+  }
+
+  @media (min-width: ${(props) => props.theme.breakpoints.desktop_small}) {
+    span {
+      display: inline-block;
+    }
+  }
+`;
+
+export const Checkbox = () => {
+  // State and handler set up for future use
+  const [isChecked, setIsChecked] = useState(false);
+
+  function handleCheck() {
+    isChecked ? setIsChecked(false) : setIsChecked(true);
+  }
+
+  return (
+    <CheckboxContainer>
+      <CheckboxInput
+        type="checkbox"
+        name="fulltime"
+        id="fulltime"
+        value="fulltime"
+        onChange={handleCheck}
+      />
+      <CheckboxLabel htmlFor="fulltime">
+        Full Time <span>Only</span>
+      </CheckboxLabel>
+    </CheckboxContainer>
+  );
+};
