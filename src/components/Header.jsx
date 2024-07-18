22dcc0013217a5d92212972d93094f3e8193c103
JC <16579792+jennyychaa@users.noreply.github.com>
2024-07-18 13:54:58 -0400

    Addressed crop and multiple image rendering in header bg; added link to the title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,24 @@
 import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
 
 import DesktopHeader from '../assets/images/header/bg-pattern-header.svg';
 import MobileHeader from '../assets/images/header/bg-pattern-header--mobile.svg';
 import TabletHeader from '../assets/images/header/bg-pattern-header--tablet.svg';
 
 const StyledHeaderContainer = styled.div`
-  width:100%;
-  height: 136px;
   display: flex;
-  background: linear-gradient(to right,rgba(0,0,0,0) 100%, ${(props) => props.theme.colors.violet} 60%), url('${MobileHeader}');
-  background-size: 100% 100%;
-  overflow:hidden;
-  background-repeat: no-repeat; 
   align-items: center;
   justify-items: center;
+  width: 100%;
+  height: 136px;
+  background-image: url('${MobileHeader}');
+  background-position: center bottom;
+  background-repeat: no-repeat;
+  background-size: cover;
 
-  @media (min-width: ${(props) => props.theme.breakpoints.tablet_small})  {
-    background: linear-gradient(to right,rgba(0,0,0,0) 10%, ${(props) => props.theme.colors.violet} 60%), url('${TabletHeader}');  
-    height:160px;
+  @media (min-width: ${(props) => props.theme.breakpoints.tablet_small}) {
+    height: 160px;
+    background-image: url('${TabletHeader}');
   }
 
   @media (min-width: ${(props) => props.theme.breakpoints.desktop_small}) {
@@ -25,18 +26,36 @@ const StyledHeaderContainer = styled.div`
   }
 `;
 
-const StyledText = styled.div`
+const StyledInnerHeaderContainer = styled.div`
+  max-width: ${({ theme }) => theme.layout.maxWidth};
+  width: 100%;
+  margin: 0 auto;
+  padding: ${(props) => `0 ${props.theme.layout.space400}`};
+  transform: translateY(-24px);
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.tablet_small}) {
+    transform: translateY(-32px);
+  }
+`;
+
+const StyledText = styled.h1`
   font-size: ${(props) => props.theme.typography.fs175};
-  font-weight: ${(props) => props.theme.typography.fwBold};
-  color: ${(props) => props.theme.colors.white};
-  transform: translateY(-30px);
-  padding-left: 80px;
+  font-weight: ${(props) => props.theme.typography.fwSemiBold};
+
+  a {
+    text-decoration: none;
+    color: ${(props) => props.theme.colors.white};
+  }
 `;
 
 const Header = () => {
   return (
     <StyledHeaderContainer>
-      <StyledText>devjobs</StyledText>
+      <StyledInnerHeaderContainer>
+        <StyledText>
+          <Link to='/'>devjobs</Link>
+        </StyledText>
+      </StyledInnerHeaderContainer>
     </StyledHeaderContainer>
   );
 };
