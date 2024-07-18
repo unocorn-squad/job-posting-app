a5ed24cdba66eeb1f0f4fb21f3db1141fc160d1c
JC <16579792+jennyychaa@users.noreply.github.com>
2024-07-18 13:54:24 -0400

    Reverted changes to the App file and added Header component to Layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import Error from './pages/Error';
 import JobDetails from './pages/JobDetails';
 import JobSearch from './pages/JobSearch';
-import Header from './components/Header';
 
 function App() {
   return (
-    // <Routes>
-    //   <Route path='/' element={<JobSearch />} />
-    //   <Route path='jobs/:jobId' element={<JobDetails />} />
-    //   <Route path='*' element={<Error />} />
-    // </Routes>
-    <Header />
-
-    
+    <Routes>
+      <Route path='/' element={<JobSearch />} />
+      <Route path='jobs/:jobId' element={<JobDetails />} />
+      <Route path='*' element={<Error />} />
+    </Routes>
   );
 }
 
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,7 @@
 import styled from '@emotion/styled';
 
+import Header from './Header';
+
 const LayoutWrapper = styled.div`
   width: 100%;
   height: 100vh;
@@ -16,14 +18,17 @@ const ContentWrapper = styled.div`
 `;
 
 const InnerContentWrapper = styled.div`
-  // @TODO Uncomment style after importing header component
-  // transform: translateY(-32px);
+  transform: translateY(-16px);
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.tablet_small}) {
+    transform: translateY(-32px);
+  }
 `;
 
 const Layout = ({ children }) => {
   return (
     <LayoutWrapper>
-      {/* @TODO Import Header component once it's complete. */}
+      <Header />
       <ContentWrapper>
         <InnerContentWrapper>{children}</InnerContentWrapper>
       </ContentWrapper>
