e88719ff9387abf26611f1c96d9668bd729d28eb
jacquikeating <j.keating@live.ca>
2024-07-22 22:00:36 -0400

    Initial setup
    - Added basic content to error page: a
    "heading", a brief error message, and a link back to the home page.
    - Added initial styles.
    - To be iterated upon later.

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,6 +1,40 @@
+import { Link } from 'react-router-dom';
+import Layout from '../../components/Layout';
+import styled from '@emotion/styled';
+
+const StyledHeading = styled.div`
+  font-size: ${(props) => props.theme.typography.fs150};
+  font-weight: ${(props) => props.theme.typography.fwSemiBold};
+  color: ${(props) => props.theme.colors.black};
+  padding-top: ${(props) => props.theme.layout.space500};
+`;
+
+const StyledText = styled.div`
+  font-size: ${(props) => props.theme.typography.fs100};
+  font-weight: ${(props) => props.theme.typography.fwRegular};
+  color: ${(props) => props.theme.colors.black};
+  padding-top: ${(props) => props.theme.layout.space200};
+`;
+
+const StyledLink = styled(Link)`
+  font-size: ${(props) => props.theme.typography.fs100};
+  font-weight: ${(props) => props.theme.typography.fwRegular};
+  color: ${(props) => props.theme.colors.black};
+  padding-top: ${(props) => props.theme.layout.space200};
+  display: block;
+  &:hover,:focus {
+    color: ${(props) => props.theme.colors.darkGray};
+  }
+`;
+
 function Error() {
-  // @TODO Replace the placeholder content with appropriate components when they are complete.
-  return <>Error Page</>;
+  return (
+    <Layout>
+      <StyledHeading>404 Error</StyledHeading>
+      <StyledText>Cannot find the page you're looking for.</StyledText>
+      <StyledLink to="/">Return to Home</StyledLink>
+    </Layout>
+    );
 }
 
 export default Error;
