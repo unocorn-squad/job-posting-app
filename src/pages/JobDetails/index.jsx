c8e197a5894d9d203fec26c5f0b52b8bce0336b2
JC <16579792+jennyychaa@users.noreply.github.com>
2024-07-24 18:30:34 -0400

    Set-up the mock data to be used on the JobDetail page

--- src/pages/JobDetails/index.jsx
+++ src/pages/JobDetails/index.jsx
@@ -1,6 +1,26 @@
+import { useParams } from 'react-router-dom';
+
+import jobsData from '../../assets/data.json';
 import Layout from '../../components/Layout';
 
 function JobDetails() {
+  const { jobId } = useParams();
+  // Subtract 1 from jobId to convert to ensures the correct array element is accessed, as array indices start at 0.
+  const {
+    company,
+    logo,
+    logoBackground,
+    position,
+    postedAt,
+    contract,
+    location,
+    website,
+    apply,
+    description,
+    requirements,
+    role,
+  } = jobsData[jobId - 1];
+
   // @TODO Replace the placeholder content with appropriate components when they are complete.
   return <Layout>Job Details Placeholder</Layout>;
 }
