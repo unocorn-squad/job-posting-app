bf1d5f279fed2b756643759f7b876aa6f17bb142
jacquikeating <j.keating@live.ca>
2024-07-25 14:11:17 -0400

    Updated styles; added button link option
    - Code formatting cleanup
    - Using job details page mockup as a style reference, adjusted font colours to match
    - Placed text inside a container with a white background
    - Added an option for a button link. In the final version, we can choose one option to keep, and delete the other.
    - Imported SearchBar component. Commented out until final design is available.

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,40 +1,86 @@
-import { Link } from 'react-router-dom';
-import Layout from '../../components/Layout';
-import styled from '@emotion/styled';
+// @TODO: Uncomment SearchBar when final version is available, to allow the user to try another search directly from the error page.
+// @NOTE: Included options for a text link or a button link. Final design can use either/or.
+
+import { Link } from "react-router-dom";
+import styled from "@emotion/styled";
+
+import Layout from "../../components/Layout";
+import SearchBar from "../../components/SearchBar";
+
+const StyledContainer = styled.div`
+  ${({ theme }) => `
+    // margin-top: ${theme.layout.space600};
+    border-radius: 8px;
+    padding: ${theme.layout.space500};
+    background-color: ${theme.colors.white};
+
+    display: flex;
+    flex-direction: column;
+    gap: ${theme.layout.space300};
+  `};
+`;
 
 const StyledHeading = styled.div`
-  font-size: ${(props) => props.theme.typography.fs150};
-  font-weight: ${(props) => props.theme.typography.fwSemiBold};
-  color: ${(props) => props.theme.colors.black};
-  padding-top: ${(props) => props.theme.layout.space500};
+  ${({ theme }) => `
+    font-size: ${theme.typography.fs150};
+    font-weight: ${theme.typography.fwSemiBold};
+    color: ${theme.colors.darkBlue};
+  `};
 `;
 
 const StyledText = styled.div`
-  font-size: ${(props) => props.theme.typography.fs100};
-  font-weight: ${(props) => props.theme.typography.fwRegular};
-  color: ${(props) => props.theme.colors.black};
-  padding-top: ${(props) => props.theme.layout.space200};
+  ${({ theme }) => `
+    font-weight: ${theme.typography.fwRegular};
+    color: ${theme.colors.darkGray};
+  `};
 `;
 
 const StyledLink = styled(Link)`
-  font-size: ${(props) => props.theme.typography.fs100};
-  font-weight: ${(props) => props.theme.typography.fwRegular};
-  color: ${(props) => props.theme.colors.black};
-  padding-top: ${(props) => props.theme.layout.space200};
-  display: block;
-  &:hover,:focus {
-    color: ${(props) => props.theme.colors.darkGray};
-  }
+  ${({ theme }) => `
+    font-weight: ${theme.typography.fwSemiBold};
+    color: ${theme.colors.violet};
+    text-decoration: none;
+
+    &:hover,
+    :focus {
+      color: ${theme.colors.lightViolet};
+    }
+  `};
+`;
+
+// Button link option. If using, replace with actual button 2 component when ready.
+const StyledButton = styled.button`
+  ${({ theme }) => `
+    border-radius: 8px;
+    border: none;
+    height: 48px;
+    width: 147px;
+    background-color: #EFF0FC;
+    font-weight: ${theme.typography.fwBold};
+    color: ${theme.colors.violet};
+
+    &:hover,
+    :focus {
+      background-color: #C5C9F4;
+    }
+  `};
 `;
 
 function Error() {
   return (
     <Layout>
-      <StyledHeading>404 Error</StyledHeading>
-      <StyledText>Cannot find the page you're looking for.</StyledText>
-      <StyledLink to="/">Return to Home</StyledLink>
+      {/* <SearchBar></SearchBar> */}
+      <StyledContainer>
+        <StyledHeading>404 Error</StyledHeading>
+        <StyledText>Cannot find the page you're looking for.</StyledText>
+        <StyledLink to="/">Return to Home</StyledLink>
+        {/* Uncomment below to see button link option */}
+        {/* <Link to="/">
+          <StyledButton>Return to Home</StyledButton>
+        </Link> */}
+      </StyledContainer>
     </Layout>
-    );
+  );
 }
 
 export default Error;
