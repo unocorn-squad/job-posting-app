683859dc669019b708aeabfa6948de874a30b99a
JC <16579792+jennyychaa@users.noreply.github.com>
2024-07-31 23:40:33 -0400

    Removed console log

--- src/components/JobDetailCard.jsx
+++ src/components/JobDetailCard.jsx
@@ -2,21 +2,21 @@ import styled from '@emotion/styled';
 import Button from './Button';
 
 const JobDetailContainer = styled.div`
-  padding: ${props => props.theme.layout.space500};
+  padding: ${(props) => props.theme.layout.space500};
   margin: 0 auto;
   border-radius: 6px;
-  background-color: ${props => props.theme.jobDetailCard.bgColor};
+  background-color: ${(props) => props.theme.jobDetailCard.bgColor};
 `;
 
 const JobDetailHeader = styled.div`
-  color: ${props => props.theme.jobDetailCard.header.color};
+  color: ${(props) => props.theme.jobDetailCard.header.color};
   display: flex;
   flex-direction: column;
   justify-content: space-between;
 
-  @media (min-width: ${props => props.theme.breakpoints.tablet_small}) {
+  @media (min-width: ${(props) => props.theme.breakpoints.tablet_small}) {
     flex-direction: row;
-    align-items: center; 
+    align-items: center;
   }
 `;
 
@@ -24,29 +24,31 @@ const ButtonContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  margin-top: ${props => props.theme.layout.space400};
+  margin-top: ${(props) => props.theme.layout.space400};
 
-  @media (min-width: ${props => props.theme.breakpoints.tablet_small}) {
+  @media (min-width: ${(props) => props.theme.breakpoints.tablet_small}) {
     width: initial;
     margin-top: 0;
   }
-`
+`;
 
 const JobTitle = styled.h1`
-  color: ${props => props.theme.jobDetailCard.title.color};
-  margin-top: ${props => props.theme.layout.space100};
-  margin-bottom: ${props => props.theme.layout.space200};
+  color: ${(props) => props.theme.jobDetailCard.title.color};
+  margin-top: ${(props) => props.theme.layout.space100};
+  margin-bottom: ${(props) => props.theme.layout.space200};
 `;
 
 const JobLocation = styled.div`
-  font-size: ${props => props.theme.typography.fs87};
-  font-weight: ${props => props.theme.typography.fwBold};
-  color: ${props => props.theme.jobDetailCard.location.color};
+  font-size: ${(props) => props.theme.typography.fs87};
+  font-weight: ${(props) => props.theme.typography.fwBold};
+  color: ${(props) => props.theme.jobDetailCard.location.color};
 `;
 
 const JobDetailDescription = styled.div`
-  p, ul, ol {
-    color: ${props => props.theme.jobDetailCard.description.color};
+  p,
+  ul,
+  ol {
+    color: ${(props) => props.theme.jobDetailCard.description.color};
   }
 `;
 
@@ -62,27 +64,29 @@ const JobDetailCard = ({
   roleContent,
   roleItems,
 }) => {
-  console.log(apply)
   return (
     <JobDetailContainer>
       <JobDetailHeader>
         <div>
-          <div>{postedAt} &#8226; {contract}</div>
+          <div>
+            {postedAt} &#8226; {contract}
+          </div>
           <JobTitle>{position}</JobTitle>
           <JobLocation>{location}</JobLocation>
         </div>
         <ButtonContainer>
-          <Button as='a' href={apply} variant='primary'>Apply Now</Button>
+          <Button as='a' href={apply} variant='primary'>
+            Apply Now
+          </Button>
         </ButtonContainer>
       </JobDetailHeader>
       <JobDetailDescription>
-
         <p>{description}</p>
 
         <h3>Requirements</h3>
         <p>{requirementsContent}</p>
         <ul>
-          {requirementsItems.map(item => (
+          {requirementsItems.map((item) => (
             <li key={item}>{item}</li>
           ))}
         </ul>
@@ -90,11 +94,10 @@ const JobDetailCard = ({
         <h3>What You Will Do</h3>
         <p>{roleContent}</p>
         <ol>
-          {roleItems.map(item => (
+          {roleItems.map((item) => (
             <li key={item}>{item}</li>
           ))}
         </ol>
-
       </JobDetailDescription>
     </JobDetailContainer>
   );
